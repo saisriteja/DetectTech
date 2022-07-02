004ac550a69ba74b1452f1508c6ce26fe4b7ee3d
saisriteja <kuppasaisriteja@gmail.com>
2022-07-02 18:40:12 +0530

    dataset

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -1,10 +1,14 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
-output = 'train.zip'
+# url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
+# output = 'train.zip'
+# gdown.download(url, output, quiet=False)
+
+
+url = 'https://drive.google.com/uc?id=1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59'
+output = 'val.zip'
 gdown.download(url, output, quiet=False)
 
-
-
-
-
+url = 'https://drive.google.com/uc?id=1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V'
+output = 'test.zip'
+gdown.download(url, output, quiet=False)
