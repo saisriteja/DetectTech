060597e5ee59a4a7b9730b5a41de3701215a3f98
saisriteja <kuppasaisriteja@gmail.com>
2022-07-04 11:22:24 +0530

    dataset

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -4,15 +4,15 @@ import os
 
 # os.mkdir('/dataset')
 
-url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
+url = 'https://googledrive.com/host/1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
 output = 'train.zip'
 gdown.download(url, output, quiet=False)
 
 
-url = 'https://drive.google.com/uc?id=1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59'
+url = 'https://googledrive.com/host/1bxK5zgLn0_L8x276eKkuYA_FzwCIjb59'
 output = 'val.zip'
 gdown.download(url, output, quiet=False)
 
-url = 'https://drive.google.com/uc?id=1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V'
+url = 'https://googledrive.com/host/1PFdW_VFSCfZ_sTSZAGjQdifF_Xd5mf0V'
 output = 'test.zip'
 gdown.download(url, output, quiet=False)
