8bce629ac194104ec6a807f052944f487a41ca27
saisriteja <kuppasaisriteja@gmail.com>
2022-07-06 23:43:01 +0530

    spec

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -8,11 +8,10 @@ import os
 
 
 id = '1YM4w1FP0gWbe_sTd_A0j2njtZLtD0SUw'
-url = 'https://drive.google.com/uc?id=1UA3BBXQcKh0zAwJYwVTTX0i3wE_Fd43y'
+url = 'https://drive.google.com/uc?id=1PYf4vEWH74JMgkmJ3lEsFqHk6TtwiqNG'
 output = 'train.zip'
 gdown.download(url, output, quiet=False)
-
-
+https://drive.google.com/file/d/1PYf4vEWH74JMgkmJ3lEsFqHk6TtwiqNG/view?usp=sharing
 
 
 # url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
