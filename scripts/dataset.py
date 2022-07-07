235b721994f07ad91aeef100de7c8d8e953e4a5f
saisriteja <kuppasaisriteja@gmail.com>
2022-07-07 10:33:35 +0530

    correction

--- scripts/dataset.py
+++ scripts/dataset.py
@@ -11,7 +11,7 @@ id = '1YM4w1FP0gWbe_sTd_A0j2njtZLtD0SUw'
 url = 'https://drive.google.com/uc?id=1PYf4vEWH74JMgkmJ3lEsFqHk6TtwiqNG'
 output = 'train.zip'
 gdown.download(url, output, quiet=False)
-https://drive.google.com/file/d/1PYf4vEWH74JMgkmJ3lEsFqHk6TtwiqNG/view?usp=sharing
+# https://drive.google.com/file/d/1PYf4vEWH74JMgkmJ3lEsFqHk6TtwiqNG/view?usp=sharing
 
 
 # url = 'https://drive.google.com/uc?id=1a2oHjcEcwXP8oUF95qiwrqzACb2YlUhn'
